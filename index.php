8996a66bfc69b05c315e15e34252be11d6d867b3
Shashanka Sharma <saz439@gmail.com>
2011-04-09 14:18:49 +0530

    My Commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Prometheus</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="header">
+	<div id="logo">
+		<h1><a href="http://www.TemplatesPerfect.com" target="_blank">Prometheus</a></h1>
+		<p>Monitor your MPLS-VPN</p>
+	</div>
+	<div id="navigation">
+		<ul>
+			<li><a href="#" class="first">Home</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Partners</a></li>
+			<li><a href="#">Outsource</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Support</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+</div>
+
+<div id="content">
+	<div id="page">
+		<div id="column1">
+			<div class="box1">
+				<h2>Network Map</h2>
+				<?php include('graph_only.php');?>
+				
+				
+			</div>
+		</div>
+		<div id="column2">
+		<h3>Quick Links</h3> 
+		<?php include('sidebar.php');?>
+		<ul id="sidemenu">
+	<h4>SNMP</h4>
+    	<li id="snmpdata"><a href="#" id="snmp">SNMP</a>
+        	<ul>
+            <li></li>
+   			<li></li>
+            <li></li>
+			<li></li>			
+   		    </ul>
+        </li>
+        <li id="interface"><a href="#">Interface</a>
+        	<ul>
+            <li></li>
+   	        <li></li>
+			<li></li>	
+   		    </ul>
+        </li>
+        
+         </ul>
+		</div>
+	</div>
+	<div style="clear: both;">&nbsp;</div>
+</div>
+<div id="footer">
+	<p>&copy;&nbsp;Copyright 2010. All Rights Reserved. template design by <a href="http://www.TemplatesPerfect.com" class="links">Free web Templates</a> </p>
+</div>
+<!-- Do not chnage below this -->
+<a href="http://www.templatesperfect.com" title="Designed by TemplatesPerfect.com"><img src="images/tp.png" alt="" width="12" height="12" border="0" /></a>
+</body>
+</html>
