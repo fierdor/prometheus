fb379b1a2ce048fc75fa9ca095522b21ee7f437b
Shashank Sabesan <shashank.sabesan@gmail.com>
2011-04-17 10:56:01 +0530

    Solved conflict

--- main_packet.php
+++ main_packet.php
@@ -6,25 +6,25 @@ $arr1 = array('ce1_1','ce1_2','ce1_3','ce2_1','ce2_2');
 $router1 = "ce1_1";
 $router2 = "ce1_3";
 $arr = findpath($router1,$router2);
-$size = sizeof($arr);
+$size = sizeof($arr[0]);
 $count = 100;
 for($i=1; $i<$size-1 ; $i++)
     {
         if($i==1)
 	{
-        setoutpackets($arr[$i], $count);
-	echo "Packets out from $arr[$i] is $count";	
+        setoutpackets($arr[0][$i], $count);
+	echo "Packets out from $arr[0][$i] is $count";	
 	}
         else if($i==size-2)
 	{
-        setinpackets($arr[$i], $count);
-	echo "Packets out from $arr[$i] is $count";
+        setinpackets($arr[0][$i], $count);
+	echo "Packets out from $arr[0][$i] is $count";
 	}        
 	else
         {
-            setinpackets($arr[$i], $count);
-            setoutpackets($arr[$i], $count);
-	    echo "Packets out and in from $arr[$i] is $count";
+            setinpackets($arr[0][$i], $count);
+            setoutpackets($arr[0][$i], $count);
+	    echo "Packets out and in from $arr[0][$i] is $count";
         }
     }
 
--- packetset.php
+++ packetset.php
@@ -52,5 +52,3 @@ else
 return false;
 }
 ?>
-
-
